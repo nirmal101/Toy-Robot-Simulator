18264f3ce77cd5a5594f7925f737f5048c9d8599
Dhanushka samarasinghe <dhanushkahere@gmail.com>
2023-04-13 20:51:10 +1000

    final touches

--- dist/Game.js
+++ dist/Game.js
@@ -1,14 +1,20 @@
 "use strict";
-console.log('Welcome to the Toy Robot Simulation');
+console.log('Welcome to the Toy Robot Simulation...');
 class Robot {
+    // Method to check if the given coordinates are within bounds
+    checkBounds(x, y) {
+        return (x < 0 || x > 4 || y < 0 || y > 4) ? true : false;
+    }
+    // Method to place the robot on the table
     place(x, y, direction) {
-        if (x < 0 || x > 4 || y < 0 || y > 4) {
-            console.log("Oops! Invalid coordinates, please pick a place inside the table");
+        if (this.checkBounds(x, y)) {
+            console.log("Invalid coordinates, X and Y Coordinates can only be between 0 and 4 to faciliate 5x5 tabletop, please try again");
             return;
         }
         this.position = { x, y, direction };
     }
     move() {
+        // Check if the robot has been placed on the table
         if (!this.position) {
             console.log("Robot has not been placed yet");
             return;
@@ -16,6 +22,7 @@ class Robot {
         const { x, y, direction } = this.position;
         let newX = x;
         let newY = y;
+        //calculate new position based on robot's facing direction
         switch (direction) {
             case "NORTH":
                 newY += 1;
@@ -30,32 +37,38 @@ class Robot {
                 newX -= 1;
                 break;
         }
-        if (newX < 0 || newX > 4 || newY < 0 || newY > 4) {
+        if (this.checkBounds(newX, newY)) {
             console.log("Move not allowed, the robot will fall from the table");
             return;
         }
+        // Update the robot's position
         this.position.x = newX;
         this.position.y = newY;
     }
+    // Method to rotate the robot right
     right() {
         this.rotate(1);
     }
+    // Method to rotate the robot left
     left() {
         this.rotate(-1);
     }
+    // Helper method to rotate the robot in the given direction
     rotate(rotationDirection) {
-        //dry
         if (!this.position) {
             console.log("Robot has not been placed yet");
             return;
         }
+        // Get current direction of the robot
         const { direction } = this.position;
-        //dry
         const directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+        // Calculate the new direction index based on the current direction and rotation direction
         const currentDirectionIndex = directions.indexOf(direction);
         const newDirectionIndex = (currentDirectionIndex + rotationDirection + directions.length) % directions.length;
+        //Update the robot's direction
         this.position.direction = directions[newDirectionIndex];
     }
+    // Method to report the current position of the robot
     report() {
         if (!this.position) {
             console.log("Robot has not been placed yet");
--- src/Game.ts
+++ src/Game.ts
@@ -1,5 +1,9 @@
-console.log('Welcome to the Toy Robot Simulation')
+console.log('Welcome to the Toy Robot Simulation...')
+
+//possible directions the robot can face
 type Direction = "NORTH" | "EAST" | "SOUTH" | "WEST";
+
+//position interface to include (x,y) coordinates and a direction
 interface Position{
     x: number;
     y: number;
@@ -7,26 +11,33 @@ interface Position{
 }
 
 class Robot {
+    // Robot's position
     private position?: Position;
 
+    // Method to check if the given coordinates are within bounds
+    private checkBounds(x:number, y:number): boolean{
+        return (x < 0 || x > 4 || y < 0 || y > 4) ? true : false;
+    }
+
+    // Method to place the robot on the table
     public place(x: number, y: number, direction: "NORTH" | "EAST" | "SOUTH" | "WEST"): void {
-        if (x < 0 || x > 4 || y < 0 || y > 4) {
-            console.log("Oops! Invalid coordinates, please pick a place inside the table");
+        if (this.checkBounds(x, y)) {
+            console.log("Invalid coordinates, X and Y Coordinates can only be between 0 and 4 to faciliate 5x5 tabletop, please try again");
             return;
         }
         this.position = { x, y, direction };
     }
 
     public move(): void {
+        // Check if the robot has been placed on the table
         if (!this.position) {
             console.log("Robot has not been placed yet");
             return;
         }
-        
         const {x, y, direction} = this.position;
         let newX = x;
         let newY = y;
-
+        //calculate new position based on robot's facing direction
         switch (direction) {
             case "NORTH":
                 newY += 1;
@@ -41,49 +52,51 @@ class Robot {
                 newX -= 1;
                 break;    
         }
-
-        if (newX < 0 || newX > 4 || newY < 0 || newY > 4 ){
+        if (this.checkBounds(newX, newY) ){
             console.log("Move not allowed, the robot will fall from the table");
             return;
         }
-
+        // Update the robot's position
         this.position.x = newX;
         this.position.y = newY;
-
     }
 
+    // Method to rotate the robot right
     public right(): void {
         this.rotate(1);
     }
 
+    // Method to rotate the robot left
     public left(): void {
         this.rotate(-1);
     }
 
+    // Helper method to rotate the robot in the given direction
     private rotate(rotationDirection: -1| 1): void {
-        //dry
         if (!this.position) {
             console.log("Robot has not been placed yet");
             return
         }
-
+        // Get current direction of the robot
         const { direction } = this.position;
-        //dry
         const directions: Direction[] = [ "NORTH", "EAST", "SOUTH", "WEST"];
+        // Calculate the new direction index based on the current direction and rotation direction
         const currentDirectionIndex = directions.indexOf(direction);
         const newDirectionIndex = (currentDirectionIndex + rotationDirection + directions.length) % directions.length;
+        //Update the robot's direction
         this.position.direction = directions[newDirectionIndex];
     }
 
+    // Method to report the current position of the robot
     public report(): void {
         if (!this.position) {
             console.log("Robot has not been placed yet");
             return;
         }
-
         const { x, y, direction} = this.position;
         console.log(`Output: ${x},${y},${direction}`);
     }
+   
 
 }
 
